35fd09dc0e410b8e04149d66a2e5fc7550f6a80e
Promise Fru <promisefru1@gmail.com>
2021-09-12 08:29:24 +0100

    updated models index.js

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 const configs = require("../config.json");
 const Sequelize = require("sequelize");
 
-var sequelize = new Sequelize(configs.database.DATABASE, configs.database.USER, configs.database.PASSWORD, {
-    host: configs.database.HOST,
+var sequelize = new Sequelize(configs.database.MYSQL_DATABASE, configs.database.MYSQL_USER, configs.database.MYSQL_PASSWORD, {
+    host: configs.database.MYSQL_HOST,
     dialect: "mysql",
     logging: false,
     // define: {
