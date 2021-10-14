7add211bb1ebd394df8510586c2407f4ab00a32f
Promise Fru <promisefru1@gmail.com>
2021-10-15 00:11:12 +0100

    UPDATED /users

--- routes/index.js
+++ routes/index.js
@@ -207,10 +207,36 @@ module.exports = (app, configs, db) => {
     app.post("/users", async (req, res, next) => {
         try {
             // ==================== REQUEST BODY CHECKS ====================
+            if (!req.body.auth_key) {
+                throw new ErrorHandler(400, "Auth_key cannot be empty");
+            };
+
+            if (!req.body.auth_id) {
+                throw new ErrorHandler(400, "Auth_id cannot be empty");
+            };
+
             if (!req.body.email) {
                 throw new ErrorHandler(400, "Email cannot be empty");
             };
             // =============================================================
+
+            // VERIFY ADMIN
+            let admin = await mysql.query(`SELECT * FROM admins WHERE auth_id = ? AND auth_key = ?`, {
+                replacements: [req.body.auth_id, req.body.auth_key],
+                type: QueryTypes.SELECT
+            }).catch(error => {
+                throw new ErrorHandler(500, error);
+            });
+
+            // RETURN = [], IF ADMIN NOT FOUND
+            if (admin.length < 1) {
+                throw new ErrorHandler(401, "Admin doesn't exist (Unauthorized)");
+            }
+
+            // IF RETURN HAS MORE THAN ONE ITEM
+            if (admin.length > 1) {
+                throw new ErrorHandler(409, "Duplicate Admins (Forbidden)");
+            }
 
             // SEARCH FOR USER IN DB
             let user = await User.findAll({
